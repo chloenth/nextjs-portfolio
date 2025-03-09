56c2d16324a74367e1f2e252e1a3b695329e5bae
Huong <huongnguyen.nth1709@gmail.com>
2025-03-09 10:23:15 +0700

    add Inter, Calistoga font as font-sans and font-serif in tailwind config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,20 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter, Calistoga } from 'next/font/google';
+import { twMerge } from 'tailwind-merge';
+
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
+const calistoga = Calistoga({
+  subsets: ['latin'],
+  variable: '--font-serif',
+  weight: ['400'],
+});
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  title: 'My Portfolio',
+  description:
+    'A dynamic and visually engaging portfolio built with Next.js, styled with Tailwind CSS, and enhanced with smooth CSS animations. Showcasing my projects, skills, and creativity with a modern, responsive, and interactive design.',
 };
 
 export default function RootLayout({
@@ -12,8 +23,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          'bg-gray-900 text-white antialiased font-sans'
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
