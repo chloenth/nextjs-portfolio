bc433eb954bf6961f27bc5d935cb3114bc05844a
Huong <huongnguyen.nth1709@gmail.com>
2025-03-10 11:53:29 +0700

    make project section responsive on desktop screen

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -45,7 +45,7 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return (
-    <section className='pb-16'>
+    <section className='pb-16 lg:py-24'>
       <div className='container'>
         <div className='flex justify-center'>
           <p className='uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text'>
@@ -56,7 +56,7 @@ export const ProjectsSection = () => {
         <h2 className='font-serif text-3xl md:text-5xl text-center mt-6'>
           Featured Projects
         </h2>
-        <p className='text-center md:text-lg text-white/60 mt-4 max-w-md mx-auto'>
+        <p className='text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto'>
           See how I transformed concepts into engaging digital experiences.
         </p>
 
@@ -66,50 +66,62 @@ export const ProjectsSection = () => {
           {portfolioProjects.map((project) => (
             <div
               key={project.title}
-              className='bg-gray-800 rounded-3xl outline outline-offset-1 outline-white/20 px-8 pt-8 md:pt-12 md:px-10 overflow-hidden relative'
+              className='bg-gray-800 rounded-3xl outline outline-offset-1 outline-white/20 px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 overflow-hidden relative'
             >
+              {/* Grain background - Project Card */}
               <div
                 className='absolute inset-0 -z-10 opacity-5'
                 style={{ backgroundImage: `url(${images.grainImage})` }}
               ></div>
-              <div className='bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text inline-flex gap-2 font-bold uppercase tracking-widest text-sm'>
-                <span>{project.company}</span>
-                <span>&bull;</span>
-                <span>{project.year}</span>
+
+              {/* Grid layout on lg screen - Project Card */}
+              <div className='lg:grid lg:grid-cols-2 lg:gap-16'>
+                {/* First column of Grid layout on lg screen - Project Card */}
+                <div className='lg:pb-16'>
+                  {/* Project Info - Project Card */}
+                  <div className='bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text inline-flex gap-2 font-bold uppercase tracking-widest text-sm'>
+                    <span>{project.company}</span>
+                    <span>&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
+
+                  <h3 className='font-serif text-2xl mt-2 md:mt-5 md:text-4xl'>
+                    {project.title}
+                  </h3>
+
+                  <hr className='border-t-2 border-white/5 mt-4 md:mt-5' />
+
+                  <ul className='flex flex-col gap-4 mt-4 md:mt-5'>
+                    {project.results.map((result) => (
+                      <li
+                        key={result.title}
+                        className='flex gap-2 text-sm md:text-base text-white/50'
+                      >
+                        <CheckCircleIcon className='size-5 md:text-size-6' />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+
+                  <a href={project.link}>
+                    <button className='bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8'>
+                      <span>Visit Live Site</span>
+                      <ArrowUpRightIcon className='size-4' />
+                    </button>
+                  </a>
+                </div>
+
+                {/* Second column of Grid layout on lg screen - Project Card */}
+                <div className='relative'>
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    width={300}
+                    height={300}
+                    className='mt-8 -mb-4 md:mb-0 w-full lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none'
+                  />
+                </div>
               </div>
-
-              <h3 className='font-serif text-2xl mt-2 md:mt-5 md:text-4xl'>
-                {project.title}
-              </h3>
-
-              <hr className='border-t-2 border-white/5 mt-4 md:mt-5' />
-
-              <ul className='flex flex-col gap-4 mt-4 md:mt-5'>
-                {project.results.map((result) => (
-                  <li
-                    key={result.title}
-                    className='flex gap-2 text-sm md:text-base text-white/50'
-                  >
-                    <CheckCircleIcon className='size-5 md:text-size-6' />
-                    <span>{result.title}</span>
-                  </li>
-                ))}
-              </ul>
-
-              <a href={project.link}>
-                <button className='bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8'>
-                  <span>Visit Live Site</span>
-                  <ArrowUpRightIcon className='size-4' />
-                </button>
-              </a>
-
-              <Image
-                src={project.image}
-                alt={project.title}
-                width={300}
-                height={300}
-                className='mt-8 -mb-4 md:mb-0 w-full'
-              />
             </div>
           ))}
         </div>
